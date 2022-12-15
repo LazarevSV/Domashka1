c91d0c047d97af0bdc629eb744f1dfbe0ee6b31b
Lazarev Sergey <grisfrost@gmail.com>
2022-12-16 00:30:31 +0300

    add1

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max =  " + num1 ); 
+} 
+else
+{
+    Console.WriteLine("max =  " + num2 );
+} 
+*/
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());  
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("Максимальное число: " + num1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + num3);
+    }
+}
+
+else if (num2 > num3)
+{
+    Console.WriteLine("Максимальное число: " + num2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + num3);
+}
+*/
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+        Console.Write("да");
+}
+else 
+{
+    Console.Write("нет");  
+}         
+*/
+
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+
+
+while(startNumber <= number)
+    {
+        if(startNumber % 2 ==0)
+        Console.Write(startNumber + ", ");
+        startNumber ++;
+    }
+    
